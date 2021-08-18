9847eeb6309e34b4e881f143106e9360804b4095
Alexander Ho <alexanderho1998@gmail.com>
2021-08-18 10:09:09 -0500

    Updates

---
+++ precache-manifest.860c32833fb8b136feab480a1eef706e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb32773c833060f8c119d84ce66f844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57378be49ebcd632f7a2",
+    "url": "/static/css/main.1bebe6d2.chunk.css"
+  },
+  {
+    "revision": "857e27ad152576c8697d",
+    "url": "/static/js/2.ed9d53b8.chunk.js"
+  },
+  {
+    "revision": "80e109d37ee38a0b1d525ee1bdf87c21",
+    "url": "/static/js/2.ed9d53b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57378be49ebcd632f7a2",
+    "url": "/static/js/main.d1d855f8.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
